40ee86df776812c51e1397f28f17fe96e1e92763
gustavoterras <gustavo_terras@terceiros.sicredi.com.br>
2018-09-01 20:10:40 -0300

    added checkin endpoint.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,22 +1,35 @@
 package hello;
 
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.CollectionReference;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.firebase.cloud.FirestoreClient;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @RestController
+@RequestMapping("/eventos")
 public class HelloController {
 
     @CrossOrigin(origins = "*")
-        @RequestMapping("/eventos")
-    public Map<String, Object> index() throws ExecutionException, InterruptedException {
+    @GetMapping("/lista")
+    public Map<String, Object> listOfEvents() throws ExecutionException, InterruptedException {
+        DocumentReference docRef = FirestoreClient.getFirestore().collection("cardume").document("eventos");
+
+        ApiFuture<DocumentSnapshot> future = docRef.get();
+
+        DocumentSnapshot document = future.get();
+
+        return document.getData();
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping("/checkin")
+    public Map<String, Object> confirmationEvent(@RequestAttribute("id") String id, @RequestAttribute("email") String email) throws ExecutionException, InterruptedException {
+
         DocumentReference docRef = FirestoreClient.getFirestore().collection("cardume").document("eventos");
 
         ApiFuture<DocumentSnapshot> future = docRef.get();
